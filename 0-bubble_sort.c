f86b4682c7d46b7517c933cf3e7cdb2dca428a0c
Osigelialex <osigelialex@gmail.com>
2023-01-18 12:21:20 -0800

    fixed function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,11 +11,11 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
-			if (arrray[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = temp;
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 
 				print_array(array, size);
 			}
