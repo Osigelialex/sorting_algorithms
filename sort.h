8f8655eebd38da4dd7aaba48afca8dd003496104
Osigelialex <osigelialex@gmail.com>
2023-01-18 12:15:30 -0800

    Modified header file

--- sort.h
+++ sort.h
@@ -15,6 +15,10 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
