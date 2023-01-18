ca084b6f80eb40aa2af0e28712273e4a9d492600
Michael Ibrahim <segunmichael24@gmail.com>
2023-01-18 21:11:24 +0100

    function for Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+/*
+ * bubble_sort - function to perform bubble sort an array
+ * @array: Integer list
+ * @size: length of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+			if (arrray[j] > array[j+1])
+			{
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+
+				print_array(array, size);
+			}
+	}
+}
